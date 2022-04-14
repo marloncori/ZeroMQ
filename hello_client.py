7efd5bd58b8dcefdcae94d824cc7c0abb9731317
Marlon Couto Ribeiro <84450525+marloncori@users.noreply.github.com>
2022-04-14 21:38:15 +0200

    Update and rename hello_publisher.py to hello_client.py

--- hello_client.py
+++ hello_client.py
@@ -1,24 +1,29 @@
 #!/usr/bin/env python3
 import time
 import zmq
-impot random
+import random
 
-def read_ultrasonic_sensor():
-  reading = random.uniform(0.05, 150.0)
+def read_SG90_pose():
+  reading = random.uniform(2.0, 180.0)
   return reading
 
 def publish_to_server():
   PORT = 5555
   context = zmq.Context()
-  publisher = context.socket(zmq.PUB)
+  publisher = context.socket(zmq.REQ)
   publisher.bin(tcp://192.168.0.59:" + str(PORT))
-  
+  counter = 1
+                
   while True:
-       distance = read_ultrasonic_sensor()
-       msg = "\n----------------------------\n The distance to object is {:.2f} cm!\n----------------------------\n".format(distance)
+       distance = read_SG90_pose()
+       msg = "\n----------------------------\n The servo motor position: {:.2f} degrees.\n----------------------------\n".format(distance)
        publisher.send_multipart([b"A", b" The distance to object is:"])
        publisher.send_string(msg)
        time.sleep(2)
+       reply = publisher.recv()
+       print("\n ===> Server has answered: \n\t reply #{} {}".format(counter, reply))
+       counter += 1
+       time.sleep(1)
  
   publisher.close()
   context.term()
